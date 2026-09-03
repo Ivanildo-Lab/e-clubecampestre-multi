7bf0b2eae815126219f3f4f731bc2ee610ac2941
Ivanildo-Lab <geficogestor@gmail.com>
2026-09-03 10:08:22 -0300

    fix: ALLOWED_HOSTS config e redesign login e-clube com glassmorphism (troca apenas bg image)

--- clube_manager/settings.py
+++ clube_manager/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY', default='django-insecure-ke^#u)p0q66(a@ktyp51a
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=True, cast=bool)
 
-ALLOWED_HOSTS = config('eclubecampestre-geficogestor.pythonanywhere.com','ALLOWED_HOSTS', default='localhost,127.0.0.1', cast=lambda v: [s.strip() for s in v.split(',')])
+ALLOWED_HOSTS = config('ALLOWED_HOSTS', default='localhost,127.0.0.1,eclubecampestre-geficogestor.pythonanywhere.com', cast=lambda v: [s.strip() for s in v.split(',')])
 
 
 # Application definition
